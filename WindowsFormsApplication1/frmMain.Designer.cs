48db798ee5a3053ec969de9f0d1a077b1907b514
Alan Drees <alandrees@theselves.com>
2013-11-26 06:49:44 -0500

    inital commmit

---
+++ WindowsFormsApplication1/frmMain.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AllowDrop = true;
+            this.lblText.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.lblText.Font = new System.Drawing.Font("Times New Roman", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblText.Location = new System.Drawing.Point(12, 9);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(210, 155);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "DRAG AND DROP FILE HERE";
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblText.DragDrop += new System.Windows.Forms.DragEventHandler(this.do_data_input);
+            // 
+            // frmMain
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(234, 173);
+            this.Controls.Add(this.lblText);
+            this.DoubleBuffered = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(250, 211);
+            this.MinimumSize = new System.Drawing.Size(250, 211);
+            this.Name = "frmMain";
+            this.Opacity = 0.5D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "SC Upload Tagger";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.do_data_input);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+    }
+}
+
---
+++ WindowsFormsApplication1/frmMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    
+    
+    public partial class frmMain : Form
+    {
+        string config_file = "C:\\sc_upload\\data";
+        
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.load_config();
+
+            
+        }
+
+        private void load_config()
+        {
+            /**\fn load_config
+             * 
+             * loads the configuration file, parsing out the required
+             * information
+             * 
+             * @param None
+             * 
+             * @returns None
+             */          
+            
+        }
+
+        private void do_data_input(object sender, DragEventArgs e)
+        {
+            MessageBox.Show("BAM");
+        }
+    }
+}
