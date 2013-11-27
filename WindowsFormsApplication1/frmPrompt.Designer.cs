ebdb3d9bf651804b98d17927178c944278354b5f
Alan Drees <alandrees@theselves.com>
2013-11-27 06:43:13 -0500

    Added the prompt form to the repository

---
+++ WindowsFormsApplication1/frmPrompt.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class frmPrompt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(11, 75);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 0;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            this.btnNext.Click += new System.EventHandler(this.btnNext_Click);
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrompt.Location = new System.Drawing.Point(12, 9);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(526, 26);
+            this.lblPrompt.TabIndex = 1;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(12, 38);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(526, 26);
+            this.txtInput.TabIndex = 2;
+            // 
+            // frmPrompt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 110);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.lblPrompt);
+            this.Controls.Add(this.btnNext);
+            this.Name = "frmPrompt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox txtInput;
+    }
+}
---
+++ WindowsFormsApplication1/frmPrompt.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class frmPrompt : Form
+    {
+
+        string selected_input;
+        
+        Dictionary<String, String> param_map = new Dictionary<String, String>();
+
+        public Dictionary<String, String> input_data = new Dictionary<String, String>();
+      
+        public frmPrompt()
+        {
+            InitializeComponent();
+
+            this.param_map.Add("title", "Track Title: ");
+            this.param_map.Add("genre", "Genre: ");
+            this.param_map.Add("sc_tags", "Soundcloud Tags (Comma separated): ");
+            this.param_map.Add("mtags", "Meta Tags: ");
+            this.param_map.Add("dls", "Make this track Downloadable? (Yes or No): ");
+            this.param_map.Add("private", "Make this track Private? (Yes or No): ");
+
+            this.input_data.Add("title", "");
+            this.input_data.Add("genre", "");
+            this.input_data.Add("sc_tags", "");
+            this.input_data.Add("mtags", "");
+            this.input_data.Add("dls", "");
+            this.input_data.Add("private", "");
+        }
+
+        private void btnNext_Click(object sender, EventArgs e){
+            this.input_data[this.selected_input] = this.txtInput.Text;
+
+            Dictionary<string,String>.KeyCollection keys = this.param_map.Keys;
+
+
+            
+        }
+    }
+}
